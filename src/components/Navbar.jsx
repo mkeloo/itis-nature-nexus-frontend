62cf13839e9593e90c6fdcfc9dcf22a5c97bbd7e
mkeloo <mkeloo@ufl.edu>
2024-03-16 00:09:26 -0400

    Frontend basic setup done. Navbar basic setup done.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from '../assets/Nature-Nexus-logo.png';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo Section */}
+        <div className="flex items-center">
+          <img src={logo} alt="Logo" className="h-10 mr-2" />
+          <span>Nature Nexus</span>
+        </div>
+
+        {/* Navigation Links */}
+        <div className="hidden md:flex space-x-4">
+          <a href="#" className="hover:text-gray-300">
+            Home
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            Link 1
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            Link 2
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            Link 3
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            Link 4
+          </a>
+        </div>
+
+        {/* Sign Up / Sign In Button */}
+        <div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Sign Up / Sign In
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
