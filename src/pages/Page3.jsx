718cf548b5b20d7c7c536e84d806094eb2d3f332
Dorian De Jesus <dorian.dejesus@ufl.edu>
2024-04-18 11:10:07 -0400

    added chart 2 to page 3

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -1,4 +1,5 @@
 import Query3Chart from '../components/QueryComponents/Query3/Query3Chart';
+import Query3Chart2 from '../components/QueryComponents/Query3/Query3Chart2';
 import Query3Chart5 from '../components/QueryComponents/Query3/Query3Chart5';
 
 const Page3 = () => {
@@ -6,6 +7,7 @@ const Page3 = () => {
     <>
       <h1>Page3</h1>
       <Query3Chart />
+      <Query3Chart2 />
       <Query3Chart5 />
     </>
   );
